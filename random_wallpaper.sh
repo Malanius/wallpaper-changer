dea3ce13af6bc1264f72c3840ba4095211ae52f9
Michal Slota <malaniusprivierre@gmail.com>
2025-10-07 21:18:15 +0200

    feat: make the changer run under omarchy

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-flatpak run --command=hydrapaper org.gabmus.hydrapaper -r
+# omarchy uses swaybg and has fd installed by default
 
+# Set to your wallpapers directory
+WALLPAPER_DIR=~/Pictures/wallpapers/waifus
+image_path="$(fd . -e jpg -e png -e jpeg -e webp "$WALLPAPER_DIR" | shuf -n 1)"
+
+pkill -x swaybg
+uwsm-app -- swaybg -i "$image_path" -m fill 2>&1 &
