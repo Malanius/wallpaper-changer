1add59d698c8d9f2b6dacb60c02265dd93313b2b
Michal Slota <malaniusprivierre@gmail.com>
2025-10-08 09:10:25 +0200

    feat: add logs

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -3,7 +3,10 @@
 
 # Set to your wallpapers directory
 WALLPAPER_DIR=~/Pictures/wallpapers/waifus
+echo "Selecting random wallpaper from $WALLPAPER_DIR"
 image_path="$(fd . -e jpg -e png -e jpeg -e webp "$WALLPAPER_DIR" | shuf -n 1)"
+echo "Selected wallpaper: $image_path"
 
 pkill -x swaybg
 uwsm-app -- swaybg -i "$image_path" -m fill 2>&1 &
+echo "Wallpaper set to $image_path"
